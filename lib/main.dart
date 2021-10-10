bad81a851b931ab745ff802ac048460ce6eb6ddb
Senthu <senthuu27@gmail.de>
2021-10-10 17:42:12 +0200

    Implement routing functionality and register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/screens/login.dart';
+import 'package:untitled1/screens/register.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,46 +9,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LV ToDo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: Container(
-          color: Theme.of(context).primaryColorDark,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Card(
-                elevation: 8,
-                margin: const EdgeInsets.all(16),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: <Widget>[
-                      const TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(labelText: 'Email'),
-                      ),
-                      const TextField(
-                        keyboardType: TextInputType.visiblePassword,
-                        decoration: InputDecoration(labelText: 'Password'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () => print('Login clicked'),
-                        child: const Text('Login'),
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(double.infinity, 36)),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return  MaterialApp(
+        title: 'LV ToDo',
+        home: const Login(),
+      routes: {
+          '/login': (context) => const Login(),
+          '/register': (context) => const Register(),
+
+      }
     );
   }
 }
---
+++ lib/screens/register.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Container(
+        color: Theme.of(context).primaryColorDark,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              elevation: 8,
+              margin: const EdgeInsets.all(16),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    const TextField(
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(labelText: 'Name'),
+                    ),
+                    const TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                    const TextField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(labelText: 'Password'),
+                    ),
+                    const TextField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(labelText: 'Confirm Password'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => print('Login clicked'),
+                      child: const Text('Register'),
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 36)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('<- Back to Login')),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
