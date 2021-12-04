01f8a9d0d8c1f6945b29a9e1343fea81a1d2f9aa
Senthu-galaxy_book <senthuranselvanathanbusiness@gmail.com>
2021-12-04 18:50:20 +0100

    Implement config variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:lvTodo/screens/login.dart';
 import 'package:lvTodo/screens/register.dart';
 import 'package:lvTodo/screens/todos.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async{
+  await dotenv.load();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
