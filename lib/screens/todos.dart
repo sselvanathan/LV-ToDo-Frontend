a903841463846514f81b313f76e85c22d1f9d951
Senthu-galaxy_book <senthuranselvanathanbusiness@gmail.com>
2021-12-11 16:51:02 +0100

    Update Todos per http request

--- lib/screens/todos.dart
+++ lib/screens/todos.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -23,14 +24,41 @@ class Todos extends StatefulWidget {
 
 class TodosState extends State<Todos> {
   late Future<List<Todo>> futureTodos;
+  late Todo selectedTodo;
+  final _formKey = GlobalKey<FormState>();
+  final todoNameController = TextEditingController();
 
   Future<List<Todo>> fetchTodos() async {
-    http.Response response = await http
-        .get(Uri.parse(dotenv.env['API_URL_GET_TODOS'] ?? 'API_URL not found'));
+    String uri = dotenv.env['API_URL_CRUD_TODOS'] ?? 'API_URL not found';
+    http.Response response = await http.get(Uri.parse(uri));
 
     List todos = jsonDecode(response.body);
 
     return todos.map((todo) => Todo.fromJson(todo)).toList();
+  }
+
+  Future saveTodo() async {
+    final form = _formKey.currentState;
+
+    if(!form!.validate()){
+      return;
+    }
+
+    String? todo = dotenv.env['API_URL_CRUD_TODOS'];
+    String? todoId = selectedTodo.id.toString();
+    String uri = todo! + todoId;
+
+    await http.put(Uri.parse(uri),
+        headers: {
+          HttpHeaders.contentTypeHeader: 'application/json',
+          HttpHeaders.acceptHeader: 'application/json',
+        },
+        body: jsonEncode({
+          'name': todoNameController.text
+        })
+    );
+
+    Navigator.pop(context);
   }
 
   @override
@@ -55,6 +83,42 @@ class TodosState extends State<Todos> {
                       Todo todo = snapshot.data![index];
                       return ListTile(
                         title: Text(todo.name),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () {
+                            selectedTodo = todo;
+                            todoNameController.text = todo.name;
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (context) {
+                                  return Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Form(
+                                        key: _formKey,
+                                        child: Column(
+                                          children: <Widget>[
+                                            TextFormField(
+                                                controller: todoNameController,
+                                                validator: (String? value){
+                                                  if (value!.isEmpty){
+                                                    return 'Enter category name';
+                                                  }
+                                                  return null;
+                                                },
+                                                decoration:
+                                                    const InputDecoration(
+                                                  border: OutlineInputBorder(),
+                                                  labelText: 'Todo name',
+                                                )),
+                                            ElevatedButton(
+                                                onPressed: () => saveTodo(),
+                                                child: const Text('Save'))
+                                          ],
+                                        ),
+                                      ));
+                                });
+                          },
+                        ),
                       );
                     });
               } else if (snapshot.hasError) {
